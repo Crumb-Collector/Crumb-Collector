0fa5f29d76cc4b9009e2365fa68a66d5f34d180c
Joshua Forster <jsg.forster@googlemail.com>
2024-03-01 11:54:34 -0700

    Removed extra buttons

--- frontend/components/instructionsComponent/index.tsx
+++ frontend/components/instructionsComponent/index.tsx
@@ -33,7 +33,7 @@ export default function InstructionsComponent() {
         </div>
       </header>
 
-      <div className={styles.buttons_container}>
+      <div >
         <form
           onSubmit={(event) =>
             handlePortfolioSubmit(
@@ -53,8 +53,8 @@ export default function InstructionsComponent() {
             }
             placeholder="Enter wallet address"
           />
-          <button className={styles.button} type="submit">
-            <p>Fetch Portfolio</p>Fetch Portfolio
+          <button type="submit">
+            Fetch Portfolio
           </button>
         </form>
 
@@ -71,46 +71,6 @@ export default function InstructionsComponent() {
             />
           </div>
         )}
-      </div>
-
-      <div className={styles.buttons_container}>
-        <a
-          target={'_blank'}
-          href={'https://createweb3dapp.alchemy.com/#components'}
-        >
-          <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-            <p>Add Components</p>
-          </div>
-        </a>
-        <a
-          target={'_blank'}
-          href={'https://createweb3dapp.alchemy.com/#templates'}
-        >
-          <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-            <p>Explore Templates</p>
-          </div>
-        </a>
-        <a
-          target={'_blank'}
-          href={'https://docs.alchemy.com/docs/create-web3-dapp'}
-        >
-          <div className={styles.button}>
-            <img
-              src="https://static.alchemyapi.io/images/cw3d/Icon%20Large/file-eye-01-l.svg"
-              width={'20px'}
-              height={'20px'}
-            />
-            <p>Visit Docs</p>
-          </div>
-        </a>
-        <a>
-          <div className={styles.button}>
-            {/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-            <p>Contribute</p>
-          </div>
-        </a>
       </div>
     </div>
   );
