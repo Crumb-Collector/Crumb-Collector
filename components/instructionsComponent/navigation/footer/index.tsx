f207a0e215b0dc366d517229e0ac640c02e0f5be
Peyton-Spencer <pas1234567@gmail.com>
2024-03-02 05:07:12 -0700

    case sensitive!

--- components/instructionsComponent/navigation/footer/index.tsx
+++ components/instructionsComponent/navigation/footer/index.tsx
@@ -3,8 +3,8 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      
-    
+
+
     </div>
   );
 }
--- components/instructionsComponent/navigation/navbar/index.tsx
+++ components/instructionsComponent/navigation/navbar/index.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import ConnectButton from "@/components/connect-button";
-import styles from "./Navbar.module.css";
+import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
